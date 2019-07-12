040bef6c4b5d95ab11f59abab65a485419f8d7bf
AlexSkor <armoury1982@gmail.com>
2019-07-13 02:57:01 +0300

    Test02

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Test!");
+        System.out.println("It Works!");
         System.out.println("Hello World!");
     }
 }
